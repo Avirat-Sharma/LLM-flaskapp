8ad801800e9d2242443545cf8d1009681c52e395
Avirat201189221 <avirat0603@gmail.com>
2025-01-29 03:43:56 +0530

    add wsgi.py file

---
+++ wsgi.py
@@ -0,0 +1,6 @@
+# wsgi.py
+
+from app import app  # Import the Flask app from your main application file (e.g., app.py)
+
+if __name__ == "__main__":
+    app.run()  # This will run the app locally if you run wsgi.py directly, though Gunicorn will handle it in production.
