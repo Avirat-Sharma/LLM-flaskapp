f2d3b795f8daffe6e3846edc21e956a0b4ac6975
Avirat201189221 <avirat0603@gmail.com>
2025-01-29 03:49:14 +0530

    add debug

--- wsgi.py
+++ wsgi.py
@@ -3,4 +3,4 @@
 from app import app  # Import the Flask app from your main application file (e.g., app.py)
 
 if __name__ == "__main__":
-    app.run()  # This will run the app locally if you run wsgi.py directly, though Gunicorn will handle it in production.
+    app.run(debug=True)  # This will run the app locally if you run wsgi.py directly, though Gunicorn will handle it in production.
